523a1632906edb657eda49076ed6e2f2c3e5d4ae
dudasadam <dudas.adam@gmail.com>
2022-03-11 14:22:03 +0100

    próba2

---
+++ app/Models/teszt1.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class teszt1 extends Model
+{
+    use HasFactory;
+}
