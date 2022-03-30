bd3fd7da5a6b40224a7f52a91c12a1d246674e20
dudasadam <dudas.adam@gmail.com>
2022-03-30 13:14:02 +0200

    bootstrap add

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('teszt1', ['name' => 'Adam']);
+    //return view('teszt1', ['name' => 'Adam']);
+    return view('index');
 });
 
 Route::get('/welcome', function () {
     return view('welcome');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
